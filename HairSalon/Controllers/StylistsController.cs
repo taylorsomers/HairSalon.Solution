ac8bcdb78d0b372b26d31b9894d4de8209af028a
Taylor Somers <tsomers@yandex.com>
2020-07-31 12:54:22 -0400

    Add boilerplate code and Index HTTPGet action

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,24 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.OrderBy(stylist => stylist.Name).ToList();
+      return View(model);
+    }
+  }
+}
