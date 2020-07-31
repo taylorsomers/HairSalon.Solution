6482d1c2fc204f490a43fb8d98e93661f8c71e3f
Taylor Somers <tsomers@yandex.com>
2020-07-31 13:51:48 -0400

    Add Client properties

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,20 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public int ClientId { get; set; }
+
+    public int StylistId { get; set; }
+
+    public string Name { get; set; }
+
+    public Color HairColor { get; set; }
+  }
+
+  public enum Color
+  {
+    black,
+    blond,
+    brown,
+    red
   }
 }
